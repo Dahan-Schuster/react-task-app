5affe4c1f30c4e7404065d0c683b3c3b3a1f9107
dahan-schuster <dan.plschuster@gmail.com>
2022-04-02 17:36:39 -0300

    feat: Server upload configuration
    Builds the GraphQL Schema with the UserResolver
    Uploads the Apollo Server

---
+++ backend/index.ts
@@ -0,0 +1,32 @@
+import "reflect-metadata";
+
+import path from 'path';
+
+import { ApolloServer } from 'apollo-server';
+import { buildSchema } from 'type-graphql'
+
+import { UserResolver } from "./src/resolvers/UserResolver";
+
+async function main() {
+
+    /**
+     * Builds the GraphqQL schema using the available Resolvers
+     */
+    const schema = await buildSchema({
+        resolvers: [
+            UserResolver
+        ],
+        emitSchemaFile: path.resolve(__dirname, 'schema.gql')
+    });
+
+    /** Creates the Apollo server to the schema */
+    const server = new ApolloServer({
+        schema
+    });
+
+    const { url } = await server.listen();
+
+    console.log(`Server running at ${url}`);
+}
+
+main();
