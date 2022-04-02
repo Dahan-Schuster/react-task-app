c13b612f0b3952a7ae6d77b9303121c1d94004d1
dahan-schuster <dan.plschuster@gmail.com>
2022-04-02 18:55:29 -0300

    feat: Apollo client configuration

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { ApolloProvider } from '@apollo/client';
+
 import App from './App'
+import { client } from './lib/apollo';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
+	</React.StrictMode>,
+	document.getElementById('root')
 )
