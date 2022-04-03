68ccb6c41a581535a1718e999e9b66ba184acb55
dahan-schuster <dan.plschuster@gmail.com>
2022-04-03 14:21:58 -0300

    feat: Integration with express and dotenv
    Uses express to expose a /graphql route and assign middlewares
    Uses dotenv to remove sensitive data from the code

--- backend/index.ts
+++ backend/index.ts
@@ -2,33 +2,56 @@ import "reflect-metadata";
 
 import path from 'path';
 
-import { ApolloServer } from 'apollo-server';
+import express from "express";
+import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql'
+import { ApolloServerLoaderPlugin } from "type-graphql-dataloader";
+import { getConnection } from "typeorm";
 
 import { UserResolver } from "./src/resolvers/UserResolver";
+import { TaskResolver } from "./src/resolvers/TaskResolver";
 
 import './src/database/';
 
+import dotenv from 'dotenv'
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
 async function main() {
+
+	/** Express will help us to manage middlewares and authentication */
+	const app = express();
 
 	/**
 	 * Builds the GraphqQL schema using the available Resolvers
 	 */
 	const schema = await buildSchema({
 		resolvers: [
-			UserResolver
+			UserResolver,
+			TaskResolver
 		],
 		emitSchemaFile: path.resolve(__dirname, 'schema.gql')
 	});
 
-	/** Creates the Apollo server to the schema */
+	/**
+	 * Creates the Apollo server
+	 * The ApolloServerLoaderPlugins helps us to load
+	 * eager releationships in GraphQL
+	 */
 	const server = new ApolloServer({
-		schema
+		schema,
+		plugins: [
+			ApolloServerLoaderPlugin({
+				typeormGetConnection: getConnection
+			}),
+		],
+		context: ({ req, res }) => ({ req, res }),
 	});
+	await server.start();
+	server.applyMiddleware({ app });
 
-	const { url } = await server.listen();
-
-	console.log(`Server running at ${url}`);
+	app.listen(4000, () => {
+		console.log(`Server running at http://localhost:4000/graphql`);
+	});
 }
 
 main();
