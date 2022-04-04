973beb06035cf621118dab566f3821e904e2d2b3
dahan-schuster <dan.plschuster@gmail.com>
2022-04-04 16:03:12 -0300

    feat: Icon variant for Button component

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -2,22 +2,39 @@ import { CSSProperties } from "react";
 import { ButtonContainer } from "./styles";
 
 interface IButtonProps {
-	title: string;
 	onClick?(): void,
+	title?: string;
+	iconButton?: boolean;
+	icon?: string;
 	variant?: "accent" | "pink",
 	type?: "button" | "submit" | "reset" | undefined;
-	styles?: CSSProperties,
+	styles?: CSSProperties;
+	align?: "center" | "left" | "right" | undefined;
+	disabled?: boolean;
+	active?: boolean;
 }
 
-const Button = ({ onClick, title, variant = "accent", type = "button", styles = {}, ...rest }: IButtonProps) => {
+const Button = ({
+	onClick,
+	title,
+	iconButton = false,
+	icon = undefined,
+	variant = "accent",
+	type = "button",
+	styles = {},
+	align,
+	active,
+	...rest
+}: IButtonProps) => {
 	return (
 		<ButtonContainer
 			type={type}
 			onClick={onClick}
-			className={variant}
+			className={`${variant} ${align || ""} ${iconButton ? 'icon' : ''} ${active ? 'active' : ''}`}
 			style={styles}
 			{...rest}
 		>
+			{(iconButton && icon) && <img src={icon} alt={title} />}
 			{title}
 		</ButtonContainer>
 	)
