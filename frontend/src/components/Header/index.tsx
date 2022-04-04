98725c721dcc5cf6dcefbd57d46ecba15cbd168a
dahan-schuster <dan.plschuster@gmail.com>
2022-04-03 23:58:22 -0300

    feat: Logout button on Header

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,13 +1,19 @@
-import { FC } from 'react'
+import { FC, useContext } from 'react'
 import logoImg from '../../assets/logo_header.svg'
+import AuthContext from '../../contexts/auth'
 import { Container, Content } from './styles'
 
 const Header: FC = () => {
+	const { signed, Logout } = useContext(AuthContext);
+
 	return (
 		<Container>
 			<Content>
 				<img src={logoImg} alt="Simple Task App" />
-				<button>Login</button>
+
+				{signed && (
+					<button onClick={Logout}>Logout</button>
+				)}
 			</Content>
 		</Container>
 	)
