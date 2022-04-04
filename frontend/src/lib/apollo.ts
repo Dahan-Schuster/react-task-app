f9a7479f97f9b6a593ebc5cd037deb468abe85bc
dahan-schuster <dan.plschuster@gmail.com>
2022-04-04 00:54:41 -0300

    feat: Apollo authorization header

--- frontend/src/lib/apollo.ts
+++ frontend/src/lib/apollo.ts
@@ -1,6 +1,25 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { lsPrefix } from '../contexts/auth';
+
+const httpLink = createHttpLink({
+	uri: 'http://localhost:4000/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+	// get the authentication token from local storage if it exists
+	const token = localStorage.getItem(`${lsPrefix}:token`);
+
+	// return the headers to the context so httpLink can read them
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : "",
+		}
+	}
+});
 
 export const client = new ApolloClient({
-	uri: 'http://localhost:4000/graphql',
+	link: authLink.concat(httpLink),
 	cache: new InMemoryCache(),
 })
