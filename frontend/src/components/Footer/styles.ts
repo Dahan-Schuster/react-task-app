34f435d392758f33e6ec92877bd376d8b0ec6f25
dahan-schuster <dan.plschuster@gmail.com>
2022-04-04 12:19:47 -0300

    fix: Footer responsiviness

--- frontend/src/components/Footer/styles.ts
+++ frontend/src/components/Footer/styles.ts
@@ -6,13 +6,18 @@ export const Container = styled.footer`
 `;
 
 export const Content = styled.div`
-	max-width: 1120px;
+	max-width: var(--base-container-width);
 	margin: 0 auto;
 	padding: 1rem 3.125rem;
 
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
+
+	@media (max-width: 425px) {
+		padding: 1rem;
+		flex-direction: column;
+	}
 
 	button {
 		background-color: var(--accent-color);
@@ -29,6 +34,11 @@ export const ColumnList = styled.div`
 	display: flex;
 	justify-content: space-between;
 	align-items: flex-start;
+
+
+	@media (max-width: 425px) {
+		margin: 1rem 0;
+	}
 
 	.column {
 		display: flex;
